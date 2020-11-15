d43aec84c4d4f2da22169b92c4791067e059c9a3
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-11-15 01:45:35 -0300

    feat: Added galery list and some images

--- pages/galeria/main.js
+++ pages/galeria/main.js
@@ -1,1 +1,59 @@
 const container = document.querySelector(".container")
+const section = document.querySelector(".product-list > ul")
+
+const images = [
+  {
+    id: 1,
+    path: '../../assets/img/Caneca.png',
+    alt: 'Caneca'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Copo.png',
+    alt: 'Copo'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Caneca.png',
+    alt: 'Caneca'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Copo.png',
+    alt: 'Copo'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Caneca.png',
+    alt: 'Caneca'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Copo.png',
+    alt: 'Copo'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Caneca.png',
+    alt: 'Caneca'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Copo.png',
+    alt: 'Copo'
+  },
+  {
+    id: 1,
+    path: '../../assets/img/Copo.png',
+    alt: 'Copo'
+  },
+]
+
+
+section.innerHTML = images.map(item => (
+  `
+    <li>
+      <img src="${item.path}" alt="${item.alt}">
+    </li>
+  `
+)).join('')
