a5ae84335cc65a3cca22741866cf75d8badb546d
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-11-17 21:54:37 -0300

    added images to galery

--- pages/galeria/main.js
+++ pages/galeria/main.js
@@ -1,59 +1,102 @@
 const container = document.querySelector(".container")
-const section = document.querySelector(".product-list > ul")
+const list = document.querySelector(".list > ul")
+const gallery_stream = document.querySelector(".gallery_stream")
 
 const images = [
   {
     id: 1,
-    path: '../../assets/img/Caneca.png',
-    alt: 'Caneca'
+    path: '../../assets/img/galeria/img1.jpg',
+    alt: 'Imagem 1'
   },
   {
-    id: 1,
-    path: '../../assets/img/Copo.png',
-    alt: 'Copo'
+    id: 2,
+    path: '../../assets/img/galeria/img2.jpg',
+    alt: 'Imagem 2'
   },
   {
-    id: 1,
-    path: '../../assets/img/Caneca.png',
-    alt: 'Caneca'
+    id: 3,
+    path: '../../assets/img/galeria/img3.jpg',
+    alt: 'Imagem 3'
   },
   {
-    id: 1,
-    path: '../../assets/img/Copo.png',
-    alt: 'Copo'
+    id: 4,
+    path: '../../assets/img/galeria/img4.jpg',
+    alt: 'Imagem 4'
   },
   {
-    id: 1,
-    path: '../../assets/img/Caneca.png',
-    alt: 'Caneca'
+    id: 5,
+    path: '../../assets/img/galeria/img5.jpg',
+    alt: 'Imagem 5'
   },
   {
-    id: 1,
-    path: '../../assets/img/Copo.png',
-    alt: 'Copo'
+    id: 6,
+    path: '../../assets/img/galeria/img6.jpg',
+    alt: 'Imagem 6'
   },
   {
-    id: 1,
-    path: '../../assets/img/Caneca.png',
-    alt: 'Caneca'
+    id: 7,
+    path: '../../assets/img/galeria/img7.jpg',
+    alt: 'Imagem 7'
   },
   {
-    id: 1,
-    path: '../../assets/img/Copo.png',
-    alt: 'Copo'
+    id: 8,
+    path: '../../assets/img/galeria/img8.jpg',
+    alt: 'Imagem 8'
   },
   {
-    id: 1,
-    path: '../../assets/img/Copo.png',
-    alt: 'Copo'
+    id: 9,
+    path: '../../assets/img/galeria/img9.jpg',
+    alt: 'Imagem 9'
+  },
+  {
+    id: 10,
+    path: '../../assets/img/galeria/img10.jpg',
+    alt: 'Imagem 10'
+  },
+  {
+    id: 11,
+    path: '../../assets/img/galeria/img11.jpg',
+    alt: 'Imagem 11'
+  },
+  {
+    id: 12,
+    path: '../../assets/img/galeria/img12.jpg',
+    alt: 'Imagem 12'
   },
 ]
 
+var focusedImg = 1;
 
-section.innerHTML = images.map(item => (
+list.innerHTML = images.map(item => (
   `
-    <li>
+    <li onclick="openImgModal(${item.id})">
       <img src="${item.path}" alt="${item.alt}">
     </li>
   `
 )).join('')
+
+gallery_stream.innerHTML = images.map(item => (
+  `
+    <div 
+    style="background-image: url('${item.path}');"
+    class="gallery_item bg-${item.id}" >
+    </div>
+  `
+)).join('')
+
+function openImgModal(imageId) {
+  console.log(imageId)
+}
+
+function nextImg() {
+  if(focusedImg === 1) {
+    focusedImg = 12
+  }
+}
+
+
+function prevImg() {
+  if (focusedImg === 12) {
+    focusedImg = 1
+  }
+}
